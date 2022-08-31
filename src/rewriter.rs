ec5383f9b076e4578696abbd80f91a91c69a751a
Igor Unanua <igor.unanua@datadoghq.com>
2022-08-31 17:12:58 +0200

    Move test method

--- src/rewriter.rs
+++ src/rewriter.rs
@@ -35,39 +35,6 @@ pub struct RewrittenOutput {
     pub original_map: Option<SourceMap>,
 }
 
-#[cfg(test)]
-pub fn debug_js(code: String) -> Result<RewrittenOutput> {
-    let compiler = Compiler::new(Arc::new(common::SourceMap::new(FilePathMapping::empty())));
-    return try_with_handler(compiler.cm.clone(), default_handler_opts(), |handler| {
-        let js_file = "debug.js".to_string();
-        let program = parse_js(code, &js_file, handler, compiler.borrow())?;
-
-        print!("{:#?}", program);
-
-        let print_result = compiler.print(
-            &program,
-            file_name(&js_file),
-            None,
-            false,
-            EsVersion::Es2020,
-            SourceMapsConfig::Bool(true),
-            &Default::default(),
-            None,
-            false,
-            None,
-        );
-
-        print_result.map(|printed| RewrittenOutput {
-            code: printed.code,
-            source_map: printed.map.unwrap(),
-            original_map: extract_source_map(
-                Path::new(js_file.as_str()).parent().unwrap(),
-                compiler.comments(),
-            ),
-        })
-    });
-}
-
 pub fn rewrite_js(code: String, file: String) -> Result<RewrittenOutput> {
     let compiler = Compiler::new(Arc::new(common::SourceMap::new(FilePathMapping::empty())));
     return try_with_handler(compiler.cm.clone(), default_handler_opts(), |handler| {
@@ -217,4 +184,37 @@ fn extract_source_map(folder: &Path, comments: &SwcComments) -> Option<SourceMap
         }
     }
     return None;
+}
+
+#[cfg(test)]
+pub fn debug_js(code: String) -> Result<RewrittenOutput> {
+    let compiler = Compiler::new(Arc::new(common::SourceMap::new(FilePathMapping::empty())));
+    return try_with_handler(compiler.cm.clone(), default_handler_opts(), |handler| {
+        let js_file = "debug.js".to_string();
+        let program = parse_js(code, &js_file, handler, compiler.borrow())?;
+
+        print!("{:#?}", program);
+
+        let print_result = compiler.print(
+            &program,
+            file_name(&js_file),
+            None,
+            false,
+            EsVersion::Es2020,
+            SourceMapsConfig::Bool(true),
+            &Default::default(),
+            None,
+            false,
+            None,
+        );
+
+        print_result.map(|printed| RewrittenOutput {
+            code: printed.code,
+            source_map: printed.map.unwrap(),
+            original_map: extract_source_map(
+                Path::new(js_file.as_str()).parent().unwrap(),
+                compiler.comments(),
+            ),
+        })
+    });
 }
