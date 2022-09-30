453af062bf0a59b518e47fbd58e7cc6a83bddf51
Igor Unanua <igor.unanua@datadoghq.com>
2022-10-01 00:12:09 +0200

    Replace template literal with a binary add expression and transform it (tests not working)

--- src/visitor/template_transform.rs
+++ src/visitor/template_transform.rs
@@ -2,89 +2,49 @@
 * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
 * This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2022 Datadog, Inc.
 **/
-use swc::{atoms::JsWord, common::Span, ecmascript::ast::*};
-
-use super::{
-    operation_transform_visitor::OperationTransformVisitor,
-    visitor_util::{get_dd_plus_operator_paren_expr, is_typeof},
-};
+use swc::{atoms::JsWord, common::DUMMY_SP, ecmascript::ast::*};
 
 pub struct TemplateTransform {}
 
 impl TemplateTransform {
-    pub fn to_dd_tpl_expr(expr: &Expr, opv: &mut OperationTransformVisitor) -> Expr {
-        match expr {
-            Expr::Tpl(tpl) => {
-                let mut tpl_clone = tpl.clone();
+    pub fn get_binary_from_tpl(tpl: &Tpl) -> Expr {
+        // at least 2 arguments (reversed): tail, expr, quasi (empty quasi `` is filtered)
+        let arguments = get_reversed_arguments(tpl);
 
-                let mut assignations = Vec::new();
-                let mut arguments = Vec::new();
-                if prepare_replace_expressions_in_template(
-                    &mut tpl_clone,
-                    &mut assignations,
-                    &mut arguments,
-                    opv,
-                    tpl.span,
-                ) {
-                    return get_dd_plus_operator_paren_expr(
-                        Expr::Tpl(tpl_clone),
-                        &arguments,
-                        &mut assignations,
-                        tpl.span,
-                    );
-                }
-            }
-            _ => {}
-        }
-        expr.clone()
-    }
-}
+        let mut binary_expr = BinExpr {
+            span: DUMMY_SP,
+            op: BinaryOp::Add,
+            left: Box::new(arguments[1].clone()),
+            right: Box::new(arguments[0].clone()),
+        };
 
-fn prepare_replace_expressions_in_template(
-    original_tpl: &mut Tpl,
-    assignations: &mut Vec<Box<Expr>>,
-    arguments: &mut Vec<Expr>,
-    opv: &mut OperationTransformVisitor,
-    span: Span,
-) -> bool {
-    let mut arguments_all = Vec::new();
-    let replace_original_tpl =
-        extract_arguments_in_template(original_tpl, assignations, &mut arguments_all, opv, span);
-    if replace_original_tpl {
-        // replace original template quasis and exprs with new expressions
-        original_tpl.quasis.clear();
-        original_tpl.exprs.clear();
-
-        // we have to filter empty template arguments
-        arguments_all.iter().for_each(|a| match a {
-            Expr::Tpl(tpl) => {
-                // here tpl always have a single quasi
-                if tpl.quasis.len() > 0 && tpl.quasis[0].cooked.clone().unwrap() != JsWord::from("")
-                {
-                    arguments.push(a.clone());
-                }
-                original_tpl.quasis.append(&mut tpl.quasis.clone())
-            }
-            expr => {
-                arguments.push(a.clone());
-                original_tpl.exprs.push(Box::new(expr.clone()))
+        arguments.iter().skip(2).for_each(|arg| {
+            binary_expr = BinExpr {
+                span: DUMMY_SP,
+                op: BinaryOp::Add,
+                left: Box::new(arg.clone()),
+                right: Box::new(Expr::Bin(binary_expr.clone())),
             }
         });
+
+        Expr::Bin(binary_expr)
     }
-    replace_original_tpl
 }
 
-fn extract_arguments_in_template(
-    tpl: &mut Tpl,
-    assignations: &mut Vec<Box<Expr>>,
-    arguments: &mut Vec<Expr>,
-    opv: &mut OperationTransformVisitor,
-    span: Span,
-) -> bool {
+fn get_reversed_arguments(tpl: &Tpl) -> Vec<Expr> {
+    let mut arguments = Vec::new();
     let mut index = 0;
-    let mut all_literals: bool = true;
-    let mut pending_idents = Vec::new();
+    let empty_quasi = JsWord::from("");
     for quasi in &tpl.quasis {
+        let value = quasi.cooked.clone();
+        if value.is_none() || value.unwrap() == empty_quasi {
+            let expr = &*tpl.exprs[index];
+            arguments.push(expr.clone());
+            index += 1;
+            continue;
+        }
+
+        // TODO: generate a Exp::Lit instead Expr::Tpl
         let mut expr_args = Vec::new();
         expr_args.push(TplElement {
             span: quasi.span,
@@ -101,56 +61,11 @@ fn extract_arguments_in_template(
 
         if !quasi.tail {
             let expr = &*tpl.exprs[index];
-            match expr {
-                Expr::Lit(_) => {
-                    arguments.push(expr.clone());
-                }
-                Expr::Unary(unary) => {
-                    let is_typeof = is_typeof(&unary);
-                    let ident = opv.get_ident_used_in_assignation_with_definitive(
-                        expr.clone(),
-                        assignations,
-                        arguments,
-                        span,
-                        !is_typeof,
-                    );
-
-                    // replace operand with new ident
-                    tpl.exprs[index] = Box::new(Expr::Ident(ident.clone()));
-
-                    if is_typeof {
-                        // the result of `typeof obj` is always a literal string so the ident is not replaced until we
-                        // know that tpl contains some other not literal expression
-                        pending_idents.push(ident);
-                    } else {
-                        all_literals = false;
-                    }
-                }
-                _ => {
-                    let ident = opv.get_ident_used_in_assignation(
-                        expr.clone(),
-                        assignations,
-                        arguments,
-                        span,
-                    );
-
-                    // replace operand with new ident
-                    tpl.exprs[index] = Box::new(Expr::Ident(ident));
-
-                    all_literals = false;
-                }
-            }
-            index += 1;
+            arguments.push(expr.clone());
         }
-    }
 
-    if !all_literals {
-        pending_idents.iter().for_each(|id| {
-            if !opv.idents.contains(id) {
-                opv.idents.push(id.clone()); // another clone :(
-            }
-        });
+        index += 1;
     }
-
-    !all_literals
+    arguments.reverse();
+    arguments
 }
