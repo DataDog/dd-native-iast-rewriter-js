f45524b9e5c2a204c66251011cebfeed7e9bd509
Igor Unanua <igor.unanua@datadoghq.com>
2022-11-04 16:13:23 +0100

    Move 'use'

--- src/transform/call_expr_transform.rs
+++ src/transform/call_expr_transform.rs
@@ -1,8 +1,8 @@
-use swc::atoms::JsWord;
 /**
  * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
  * This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2022 Datadog, Inc.
  **/
+use swc::atoms::JsWord;
 use swc_ecma_visit::swc_ecma_ast::{
     CallExpr, Callee, Expr, ExprOrSpread, Ident, MemberExpr, MemberProp,
 };
