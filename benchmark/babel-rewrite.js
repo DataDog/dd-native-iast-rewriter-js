dc23e14f6c2d6fbfae88a477f9cba476fd79ed95
Igor Unanua <igor.unanua@datadoghq.com>
2022-11-16 15:47:13 +0100

    More suggestions

--- benchmark/babel-rewrite.js
+++ benchmark/babel-rewrite.js
@@ -1,3 +1,8 @@
+/**
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2.0 License.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/). Copyright 2022 Datadog, Inc.
+ **/
+
 const babelParser = require('@babel/parser')
 const babelParse = babelParser.parse
 const { parse, print } = require('recast')
