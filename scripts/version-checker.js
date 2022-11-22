d0933e4bf326e5ba0a290ba31ffd6f778e3fee95
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2022-11-22 17:42:44 +0100

    Check versions of package and cargo and license-3rdparty  (#34)

---
+++ scripts/version-checker.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-console */
+const fs = require('fs')
+const path = require('path')
+const packageJson = require('../package.json')
+const packageLockJson = require('../package-lock.json')
+const cargoToml = fs.readFileSync(path.join(__dirname, '..', 'Cargo.toml')).toString()
+const cargoLock = fs.readFileSync(path.join(__dirname, '..', 'Cargo.lock')).toString()
+
+const definedVersion = packageJson.version
+const packageLockVersion = packageLockJson.version
+let isOk = true
+if (definedVersion !== packageLockVersion) {
+  isOk = false
+}
+const cargoTomlVersion = readVersionFromCargoToml(cargoToml)
+if (definedVersion !== cargoTomlVersion) {
+  isOk = false
+}
+
+const cargoLockVersion = readVersionFromCargoLock(cargoLock)
+if (definedVersion !== cargoLockVersion) {
+  isOk = false
+}
+
+if (!isOk) {
+  console.error(`Version are not synchronized:
+  package.json: ${definedVersion}
+  package-lock.json: ${packageLockVersion}
+  Cargo.toml: ${cargoTomlVersion}
+  Cargo.lock: ${cargoLockVersion}
+  `)
+  process.exit(1)
+}
+
+function readVersionFromCargoToml (cargoTomlContent) {
+  const lines = cargoTomlContent.split('\n')
+  let inPackage = false
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim()
+    if (inPackage) {
+      if (line.indexOf('version') === 0) {
+        return JSON.parse(line.split('=')[1].trim())
+      }
+    }
+    if (line === '[package]') {
+      inPackage = true
+    }
+  }
+}
+
+function readVersionFromCargoLock (cargoLockContent) {
+  const lines = cargoLockContent.split('\n')
+  let inPackage = false
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim()
+    if (inPackage) {
+      if (line.indexOf('version =') === 0) {
+        return JSON.parse(line.split('=')[1].trim())
+      }
+    } else if (line === 'name = "native-iast-rewriter"') {
+      inPackage = true
+    }
+  }
+}
