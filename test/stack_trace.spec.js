17f1ff96c7918b3a30427f1044af0ad64cce8d14
Igor Unanua <igor.unanua@datadoghq.com>
2022-12-28 22:16:29 +0100

    Use rewrittenSourceMapsCache

--- test/stack_trace.spec.js
+++ test/stack_trace.spec.js
@@ -62,6 +62,16 @@ describe('V8 prepareStackTrace', () => {
 const sourceMapResourcesPath = path.join(__dirname, 'resources', 'stacktrace-sourcemap')
 
 describe('getFilenameFromSourceMap', () => {
+  const nodeSourceMap = require('../js/source-map/node_source_map')
+
+  const readFileSync = function (filename) {
+    if (filename.indexOf('.map') > 0) {
+      return fs.readFileSync(path.join(sourceMapResourcesPath, path.basename(filename)))
+    } else if (filename.indexOf('.js') > 0) {
+      return fs.readFileSync(path.join(sourceMapResourcesPath, path.basename(filename)))
+    }
+  }
+
   it('should return original object if file does not exist', () => {
     const originalPathAndLine = {
       path: path.join(sourceMapResourcesPath, 'does-not-exist.js'),
@@ -73,79 +83,60 @@ describe('getFilenameFromSourceMap', () => {
   })
 
   it('should translate with map file', () => {
+    const fileName = 'test-file.js'
     const originalPathAndLine = {
-      path: path.join(sourceMapResourcesPath, 'test-file.js'),
+      path: path.join(sourceMapResourcesPath, fileName),
       line: 5
     }
+
+    const { getSourcePathAndLineFromSourceMaps, cacheRewrittenSourceMap } = proxyquire('../js/source-map', {
+      './node_source_map': nodeSourceMap,
+      fs: { readFileSync }
+    })
+
+    const fileContent = fs.readFileSync(path.join(sourceMapResourcesPath, fileName)).toString()
+    cacheRewrittenSourceMap(originalPathAndLine.path, fileContent)
+
     const pathAndLine = getSourcePathAndLineFromSourceMaps(originalPathAndLine.path, originalPathAndLine.line, 12)
     expect(pathAndLine.path).to.be.equals(path.join(sourceMapResourcesPath, 'test-file.ts'))
     expect(pathAndLine.line).to.be.equals(2)
   })
 
   it('should translate with inlined map', () => {
+    const fileName = 'test-inline.js'
     const originalPathAndLine = {
-      path: path.join(sourceMapResourcesPath, 'test-inline.js'),
+      path: path.join(sourceMapResourcesPath, fileName),
       line: 5
     }
+    const { getSourcePathAndLineFromSourceMaps, cacheRewrittenSourceMap } = proxyquire('../js/source-map', {
+      './node_source_map': nodeSourceMap,
+      fs: { readFileSync }
+    })
+
+    const fileContent = fs.readFileSync(path.join(sourceMapResourcesPath, fileName)).toString()
+    cacheRewrittenSourceMap(originalPathAndLine.path, fileContent)
+
     const pathAndLine = getSourcePathAndLineFromSourceMaps(originalPathAndLine.path, originalPathAndLine.line, 10)
     expect(pathAndLine.path).to.be.equals(path.join(sourceMapResourcesPath, 'test-inline.ts'))
     expect(pathAndLine.line).to.be.equals(2)
   })
 
   it('should translate minified file with correct column', () => {
+    const fileName = 'test-min.min.js'
     const originalPathAndLine = {
-      path: path.join(sourceMapResourcesPath, 'test-min.min.js'),
+      path: path.join(sourceMapResourcesPath, fileName),
       line: 1
     }
-    const pathAndLine = getSourcePathAndLineFromSourceMaps(originalPathAndLine.path, originalPathAndLine.line, 23)
-    expect(pathAndLine.path).to.be.equals(path.join(sourceMapResourcesPath, 'test-min.js'))
-    expect(pathAndLine.line).to.be.equals(2)
-  })
-})
-
-describe('getFilenameFromSourceMap cache', () => {
-  const nodeSourceMap = require('../js/source-map/node_source_map')
-  afterEach(() => {
-    sinon.restore()
-  })
-
-  it('should not create two SourceMap file for the same file', () => {
-    const sourceMapSpy = sinon.spy(nodeSourceMap, 'SourceMap')
-    const { getSourcePathAndLineFromSourceMaps } = proxyquire('../js/source-map', {
-      './node_source_map': nodeSourceMap
-    })
-
-    const originalPathAndLine = {
-      path: path.join(sourceMapResourcesPath, 'test-file.js'),
-      line: 5
-    }
-    getSourcePathAndLineFromSourceMaps(originalPathAndLine.path, originalPathAndLine.line, 0)
-    getSourcePathAndLineFromSourceMaps(originalPathAndLine.path, originalPathAndLine.line, 0)
-    // eslint-disable-next-line no-unused-expressions
-    expect(sourceMapSpy).to.have.been.calledOnce
-  })
-
-  it('should has a maximum cached items', () => {
-    const sourceMapSpy = sinon.spy(nodeSourceMap, 'SourceMap')
-    const readFileSync = function (filename) {
-      if (filename.indexOf('.map') > 0) {
-        return fs.readFileSync(path.join(sourceMapResourcesPath, 'test-file.js.map'))
-      } else if (filename.indexOf('.js') > 0) {
-        return fs.readFileSync(path.join(sourceMapResourcesPath, 'test-file.js'))
-      }
-    }
-
-    const { getSourcePathAndLineFromSourceMaps } = proxyquire('../js/source-map', {
+    const { getSourcePathAndLineFromSourceMaps, cacheRewrittenSourceMap } = proxyquire('../js/source-map', {
       './node_source_map': nodeSourceMap,
       fs: { readFileSync }
     })
 
-    for (let i = 0; i < 101; i++) {
-      getSourcePathAndLineFromSourceMaps(`/source/file-${i}.js`, 5) // new entry
-      getSourcePathAndLineFromSourceMaps(`/source/file-${i}.js`, 5) // from cache
-    }
-    expect(sourceMapSpy).to.have.been.callCount(101)
-    getSourcePathAndLineFromSourceMaps('/source/file-0.js', 5)
-    expect(sourceMapSpy).to.have.been.callCount(102)
+    const fileContent = fs.readFileSync(path.join(sourceMapResourcesPath, fileName)).toString()
+    cacheRewrittenSourceMap(originalPathAndLine.path, fileContent)
+
+    const pathAndLine = getSourcePathAndLineFromSourceMaps(originalPathAndLine.path, originalPathAndLine.line, 23)
+    expect(pathAndLine.path).to.be.equals(path.join(sourceMapResourcesPath, 'test-min.js'))
+    expect(pathAndLine.line).to.be.equals(2)
   })
 })
