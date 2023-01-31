b19b6a6ac5f5682038f80cc4549b59c9057bb0e7
Igor Unanua <igor.unanua@datadoghq.com>
2023-01-31 18:07:32 +0100

    more test debug & update integration tests rewriter config

--- integration-test/setup.js
+++ integration-test/setup.js
@@ -20,10 +20,28 @@ chai.use(sinonChai)
 global.expect = chai.expect
 global.sinon = sinon
 
+const CSI_METHODS = [
+  { src: 'plusOperator', operator: true },
+  { src: 'substring' },
+  { src: 'trim' },
+  { src: 'trimStart' },
+  { src: 'trimEnd' },
+  { src: 'trimLeft' },
+  { src: 'trimRight' },
+  { src: 'toLowerCase' },
+  { src: 'toLocaleLowerCase' },
+  { src: 'toUpperCase' },
+  { src: 'toLocaleUpperCase' },
+  { src: 'replace' },
+  { src: 'replaceAll' },
+  { src: 'slice' },
+  { src: 'concat' }
+]
+
 initRewriter()
 
 function initRewriter () {
-  rewriter = new Rewriter()
+  rewriter = new Rewriter({ csiMethods: CSI_METHODS, telemetryVerbosity: 'Debug' })
   if (rewriter) {
     Object.defineProperty(global.Error, 'prepareStackTrace', getPrepareStackTraceAccessor())
     Module.prototype._compile = getCompileMethodFn(Module.prototype._compile)
@@ -48,7 +66,7 @@ function getCompileMethodFn (compileMethod) {
       if (filename.indexOf(path.join('integration-test', 'requires')) > -1) {
         const response = rewriter.rewrite(content, filename)
         // eslint-disable-next-line no-console
-        console.log(response)
+        console.log(rewriter.rewrite.toString(), response)
         content = response.content
         addEditedFile(filename)
       }
