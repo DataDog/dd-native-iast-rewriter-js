3ff3ac6448cc88f3e8471e38c9ba3db868c92ffa
Ugaitz Urien <ugaitz.urien@datadoghq.com>
2024-11-26 14:01:17 +0100

    Add executoin tests

--- test/optional_chaining.spec.js
+++ test/optional_chaining.spec.js
@@ -4,10 +4,10 @@
  **/
 /* eslint-disable no-multi-str */
 
-const { rewriteAndExpectNoTransformation, rewriteAndExpect } = require('./util')
+const { rewriteAndExpectNoTransformation, rewriteAndExpect, rewriteAst } = require('./util')
 
 describe('Optional chaining', () => {
-  describe('substring', () => {
+  describe('Rewrites', () => {
     it('does not rewrite if it is not necessary', () => {
       const js = 'a?.customMethod(1);'
 
@@ -230,7 +230,7 @@ _ddiast.stringSubstring(__datadog_test_5.call(__datadog_test_4, 2), __datadog_te
       )
     })
 
-    it('should modify a?().b.substring(1)', () => {
+    it('should modify a?.().b.substring(1)', () => {
       const js = 'a?.().b.substring(1)'
 
       rewriteAndExpect(
@@ -243,6 +243,72 @@ __datadog_test_2, __datadog_test_1, 1)));
 }`,
         false
       )
+    })
+
+    it('should modify ({})?.substring(1)', () => {
+      const js = '({})?.substring(1)'
+
+      rewriteAndExpect(
+        js,
+        `{
+let __datadog_test_0, __datadog_test_1, __datadog_test_2;
+(__datadog_test_0 = ({}), __datadog_test_0 == null ? undefined : (__datadog_test_1 = __datadog_test_0, \
+__datadog_test_2 = __datadog_test_1.substring, _ddiast.stringSubstring(__datadog_test_2.call(__datadog_test_1, 1), \
+__datadog_test_2, __datadog_test_1, 1)));
+}`,
+        false
+      )
+    })
+  })
+
+  describe('Executions', () => {
+    function compareExecutions (functionContent) {
+      const code = `(function test () {${functionContent}})()`
+
+      // eslint-disable-next-line no-eval
+      const originalResult = eval(code)
+      // eslint-disable-next-line no-eval
+      const modifiedResult = eval(rewriteAst(code))
+
+      expect(modifiedResult).to.be.eq(originalResult)
+    }
+
+    const FUNCTION_CONTENTS_TO_TEST = [
+      'const a = null; return a?.substring(1)',
+      'const a = "abcd"; return a?.substring(1)',
+      'const a = null; return a?.substring?.(1)',
+      'const a = "abcd"; return a?.substring?.(1)',
+      'const a = {}; return a.b?.substring(1)',
+      'const a = { b: "bbbb" }; return a.b?.substring(1)',
+      'const a = { b: () => {} }; return a.b()?.substring(1)',
+      'const a = { b: () => "bbb" }; return a.b()?.substring(1)',
+      'const a = {}; return a.b?.substring(1)?.substring(2)',
+      'const a = { b: "bbbbbb" }; return a.b?.substring(1)?.substring(2)',
+      'const a = {}; return a.b?.substring(1).charCodeAt(2)',
+      'const a = { b: "bbbbbb" }; return a.b?.substring(1).charCodeAt(2)',
+      'const a = {}; return a.b?.substring(1).length.toString()',
+      'const a = { b: "bbbbbb" }; return a.b?.substring(1).length.toString()',
+      'const a = null; return a?.().substring(1)',
+      'const a = () => "bbb"; return a?.().substring(1)',
+      'const b = { c: "c" }; const a = { substring: () => b }; delete a?.substring().c; return b.c;'
+    ]
+
+    FUNCTION_CONTENTS_TO_TEST.forEach((functionContent) => {
+      before(() => {
+        global._ddiast = {
+          stringSubstring (res) {
+            return res
+          }
+        }
+      })
+
+      after(() => {
+        delete global._ddiast
+      })
+
+      it('test execution: ' + functionContent, () => {
+        compareExecutions(functionContent)
+      })
     })
   })
 })
