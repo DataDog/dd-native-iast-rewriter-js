2f311e5022ae58d5e8d1f4cf473a00446a947a42
Igor Unanua <igor.unanua@datadoghq.com>
2024-12-12 10:06:05 +0100

    Two new tests

--- src/tests/arrow_func_tests.rs
+++ src/tests/arrow_func_tests.rs
@@ -55,6 +55,36 @@ mod tests {
     }
 
     #[test]
+    fn test_arrow_object_not_modified() -> Result<(), String> {
+        let original_code = "{const a = (arg) => ({ test: true }) }".to_string();
+        let js_file = "test.js".to_string();
+        let rewritten = rewrite_js(original_code, js_file).map_err(|e| e.to_string())?;
+
+        assert_that(
+            &rewritten
+                .transform_status
+                .is_some_and(|status| status.status == Status::NotModified),
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_arrow_object_2_not_modified() -> Result<(), String> {
+        let original_code = "{const a = (arg) => { test: true } }".to_string();
+        let js_file = "test.js".to_string();
+        let rewritten = rewrite_js(original_code, js_file).map_err(|e| e.to_string())?;
+
+        assert_that(
+            &rewritten
+                .transform_status
+                .is_some_and(|status| status.status == Status::NotModified),
+        );
+
+        Ok(())
+    }
+
+    #[test]
     fn test_arrow_modified() -> Result<(), String> {
         let original_code = "{const a = (arg) => arg ? `hello ${arg}` : '' }".to_string();
         let js_file = "test.js".to_string();
