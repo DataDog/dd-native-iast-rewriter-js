f7ce8414c752b80deccd15a9ded5586219a29c28
Igor Unanua <igor.unanua@datadoghq.com>
2025-01-13 10:40:09 +0100

    Fix conflict

--- main.js
+++ main.js
@@ -31,20 +31,11 @@ class NonCacheRewriter {
   }
 
   rewrite (code, file) {
-    return this.nativeRewriter.rewrite(code, file)
     const response = this.nativeRewriter.rewrite(code, file)
 
-    try {
-      const { metrics, content } = response
-      const status = metrics?.status
-      if (status === 'modified') {
-        cacheRewrittenSourceMap(file, content)
-      } else if (status === 'notmodified') {
-        // rewrite returns an empty content when for the 'notmodified' status
-        response.content = code
-      }
-    } catch (e) {
-      this.logError(e)
+    // rewrite returns an empty content when for the 'notmodified' status
+    if (response?.metrics?.status === 'notmodified') {
+      response.content = code
     }
 
     return response
